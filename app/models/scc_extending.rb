806e9d178624733f52f7e392804868a6c11a4cf3
Matthias Dellweg <dellweg@atix.de>
2017-11-02 15:53:04 +0100

    Catch RecordNotFound when assigning extensions
    and log error

--- app/models/scc_extending.rb
+++ app/models/scc_extending.rb
@@ -1,4 +1,4 @@
 class SccExtending < ApplicationRecord
-  belongs_to :scc_product
-  belongs_to :scc_extension, class_name: :SccProduct
+  belongs_to :scc_product, inverse_of: :scc_extendings
+  belongs_to :scc_extension, class_name: :SccProduct, inverse_of: :inverse_scc_extendings
 end
