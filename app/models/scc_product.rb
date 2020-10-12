a67c526a422176441054382a3de4053085f02855
Nadja Heitmann <heitmann@atix.de>
2020-10-12 08:29:10 +0000

    Functionality to sort products that have already been loaded fromw the repository.

--- app/models/scc_product.rb
+++ app/models/scc_product.rb
@@ -2,7 +2,7 @@ class SccProduct < ApplicationRecord
   include Authorizable
   include ForemanTasks::Concerns::ActionSubject
   
-  scope :only_products_with_repos, -> { joins(:scc_repositories).distinct.order(:friendly_name) }
+  scope :only_products_with_repos, -> { joins(:scc_repositories).distinct }
 
   self.include_root_in_json = false
 
