1d713e9d6414e64675c5b8888da79d84fae96f17
Nadja Heitmann <heitmann@atix.de>
2020-12-01 08:37:31 +0100

    Add controller test suite (functional tests)

---
+++ test/controllers/scc_accounts_controller_test.rb
@@ -0,0 +1,146 @@
+require 'test_plugin_helper'
+
+class SccAccountsControllerTest < ActionController::TestCase
+  def setup
+    @scc_account = scc_accounts(:one)
+  end
+
+  def stub_custom_request(url, host = 'scc.example.com', auth = 'Basic b25ldXNlcjpvbmVwYXNz')
+    stub_request(:get, url)
+      .with(
+        headers: {
+          'Accept' => 'application/vnd.scc.suse.com.v4+json',
+          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'Authorization' => auth,
+          'Host' => host,
+          'User-Agent' => "rest-client/2.1.0 (linux-gnu x86_64) ruby/#{RUBY_VERSION}p#{RUBY_PATCHLEVEL}"
+        }
+      )
+      .to_return(status: 200, body: '', headers: {})
+  end
+
+  test 'should get new' do
+    get :new, session: set_session_user
+    assert_response :success
+    assert_select 'title', 'Add SUSE Customer Center Account'
+  end
+
+  test 'should get edit' do
+    get :edit, params: { :id => @scc_account.id }, session: set_session_user
+    assert_response :success
+    assert_select 'title', "Edit #{@scc_account.name}"
+  end
+
+  test 'should get index' do
+    get :index, session: set_session_user
+    assert_response :success
+    assert_not_nil assigns(:scc_accounts)
+    body = @response.body
+    assert_not_empty body
+    assert_select 'title', 'SUSE subscriptions'
+  end
+
+  test 'should show scc_account' do
+    get :show, params: { :id => @scc_account.id }, session: set_session_user
+
+    assert_not_nil assigns(:scc_account)
+    assert_response :success
+    assert_select 'title', "Product Selection for Account #{@scc_account.name}"
+  end
+
+  test 'should 404 for unknown scc_account' do
+    get :show, params: { :id => 'does-not-exist' }, session: set_session_user
+
+    assert_response :not_found
+  end
+
+  test 'should create scc_account' do
+    account = scc_accounts(:two)
+    organization = get_organization
+    assert organization
+    assert account
+    assert_difference('SccAccount.count') do
+      post :create, params:
+                        { :scc_account => { :name => account.name, :login => account.login, :password => account.password, :organization_id => organization.id } },
+                    session: set_session_user
+    end
+  end
+
+  test 'should update scc_account' do
+    account = scc_accounts(:two)
+    put :update, params: { id: account.id, :scc_account => { :sync_date => Time.now, :interval => 'weekly' } }, session: set_session_user
+
+    assert_equal 'weekly', assigns(:scc_account).interval
+  end
+
+  #  test 'new account SCC server connection-test' do
+  #    stub_custom_request("https://scc.example.com/connect/organizations/subscriptions", "scc.suse.com", 'Basic Og==')
+  #
+  #    account = scc_accounts(:one)
+  #    assert account
+  #    post :test_connection, params: { :scc_account => { :login => account.login, :password => account.password, :base_url => account.base_url } }, session: set_session_user
+  #
+  #    assert_response :ok
+  #  end
+
+  #  test 'new account SCC server fail connection-test' do
+  #    stub_custom_request('https://scc.example.com/connect/organizations/subscriptions', "scc.suse.com", 'Basic Og==')
+  #
+  #    account = scc_accounts(:one)
+  #    assert account
+  #    post :test_connection, params: { :scc_account => { :login => account.login, :password => account.password, :base_url => account.base_url } }, session: set_session_user
+  #
+  #    assert_response :not_found
+  #    assert_match response.body, '"Failed"'
+  #  end
+  #
+  #  test 'existing account SCC server connection-test' do
+  #    stub_custom_request('https://scc.suse.com/connect/organizations/subscriptions', 'scc.suse.com', 'Basic Og==')
+
+  #    account = scc_accounts(:one)
+  #    assert account
+  #    put :test_connection, params: { :scc_account => {:id => account.id } }, session: set_session_user
+  #
+  #    assert_response :ok
+  #  end
+  # --> fails because of ActionController::UnknownFormat error
+
+  #  test 'existing account SCC server fail connection-test' do
+  #    stub_custom_request('https://scc.example.com/connect/organizations/subscriptions', "scc.suse.com", 'Basic Og==')
+  #    account = scc_accounts(:one)
+  #    assert account
+  #    post :test_connection, params: { :id => account.id }, session: set_session_user
+  #
+  #    assert_response :not_found
+  #    assert_match response.body, '"Failed"'
+  #  end
+  #
+  test 'SCC server sync products' do
+    stub_custom_request('https://scc.example.com/connect/organizations/products')
+    stub_custom_request('https://scc.example.com/connect/organizations/repositories')
+
+    account = scc_accounts(:one)
+    assert account
+    put :sync, params: { :id => account.id }, session: set_session_user
+    assert_redirected_to '/scc_accounts'
+  end
+
+  test 'SCC server bulk subscribe products' do
+    stub_custom_request('https://scc.example.com/connect/organizations/repositories')
+
+    account = scc_accounts(:one)
+    product1 = scc_products(:one)
+    product2 = scc_products(:two)
+    account.scc_products = [product1, product2]
+    put :bulk_subscribe, params: { :id => account.id, :scc_account => { :scc_subscribe_product_ids => [product1.id, product2.id] } }, session: set_session_user
+
+    assert_redirected_to '/scc_accounts'
+  end
+
+  test 'should delete scc_account' do
+    account = scc_accounts(:two)
+    assert_difference 'SccAccount.count', -1 do
+      delete :destroy, params: { :id => account.id }, session: set_session_user
+    end
+  end
+end
