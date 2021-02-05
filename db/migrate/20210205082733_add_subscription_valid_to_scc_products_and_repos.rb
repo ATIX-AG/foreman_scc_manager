18a8f2ea981ee0cd58b52ae03d0cf4ec28118cab
Nadja Heitmann <nadjah@atix.de>
2021-02-05 14:29:22 +0100

    Add database flag for subscription status of product and repository

---
+++ db/migrate/20210205082733_add_subscription_valid_to_scc_products_and_repos.rb
@@ -0,0 +1,6 @@
+class AddSubscriptionValidToSccProductsAndRepos < ActiveRecord::Migration[5.2]
+  def change
+    add_column :scc_products, :subscription_valid, :boolean, null: true
+    add_column :scc_repositories, :subscription_valid, :boolean, null: true
+  end
+end
