307cf4e9ec1d7286db527f67ad51ba7a018ee91c
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2023-11-28 22:22:50 +0100

    i18n - pulling from tx

---
+++ app/assets/javascripts/foreman_scc_manager/locale/zh_CN/foreman_scc_manager.js
@@ -0,0 +1,592 @@
+ locales['foreman_scc_manager'] = locales['foreman_scc_manager'] || {}; locales['foreman_scc_manager']['zh_CN'] = {
+  "domain": "foreman_scc_manager",
+  "locale_data": {
+    "foreman_scc_manager": {
+      "": {
+        "Project-Id-Version": "foreman_scc_manager 1.0.0",
+        "Report-Msgid-Bugs-To": "",
+        "PO-Revision-Date": "2019-10-17 13:28+0000",
+        "Last-Translator": "Amit Upadhye <aupadhye@redhat.com>, 2022",
+        "Language-Team": "Chinese (China) (https://www.transifex.com/foreman/teams/114/zh_CN/)",
+        "MIME-Version": "1.0",
+        "Content-Type": "text/plain; charset=UTF-8",
+        "Content-Transfer-Encoding": "8bit",
+        "Language": "zh_CN",
+        "Plural-Forms": "nplurals=1; plural=0;",
+        "lang": "zh_CN",
+        "domain": "foreman_scc_manager",
+        "plural_forms": "nplurals=1; plural=0;"
+      },
+      "List all scc_accounts": [
+        "列出所有 scc_accounts"
+      ],
+      "Show scc_account": [
+        "显示 scc_account"
+      ],
+      "Name of the scc_account": [
+        "scc_account 的名称"
+      ],
+      "Login id of scc_account": [
+        "scc_account 的登录 ID"
+      ],
+      "Password of scc_account": [
+        "scc_account 的密码"
+      ],
+      "URL of SUSE for scc_account": [
+        "scc_account 的 SUSE URL"
+      ],
+      "Interval for syncing scc_account": [
+        "同步 scc_account 的时间间隔"
+      ],
+      "The default download policy for repositories which were created using this SCC Account.": [
+        ""
+      ],
+      "The default mirroring policy for repositories which were created using this SCC Account.": [
+        ""
+      ],
+      "Date and time relative to which the sync interval is run": [
+        "运行同步间隔的日期和时间"
+      ],
+      "Associated GPG key of scc_account": [
+        "scc_account 的关联 GPG 密钥"
+      ],
+      "Create an scc_account": [
+        "创建 scc_account"
+      ],
+      "Update scc_account": [
+        "更新 scc_account"
+      ],
+      "Delete scc_account": [
+        "删除 scc_account"
+      ],
+      "Test connection for scc_account": [
+        "测试 scc_account 的连接"
+      ],
+      "Sync scc_account": [
+        "同步 scc_account"
+      ],
+      "Bulk subscription of scc_products for scc_account": [
+        "scc_account 的批量 scc_products 订阅"
+      ],
+      "Bulk subscription of scc_products with individual repository selection for scc_account.": [
+        ""
+      ],
+      "The selected products cannot be found for this SCC account.": [
+        ""
+      ],
+      "List products for an scc_account": [
+        ""
+      ],
+      "Show subscribed products only": [
+        ""
+      ],
+      "Show an scc_account product": [
+        "显示 scc_account 产品"
+      ],
+      "Subscribe product": [
+        "订阅产品"
+      ],
+      "Sync task started.": [
+        "同步任务已启动。"
+      ],
+      "Failed to add task to queue: %s": [
+        "向队列添加任务失败：%s"
+      ],
+      "Lock on SCC account already taken: %s": [
+        "锁定 SCC 帐户已采用：%s"
+      ],
+      "Task to subscribe products started.": [
+        "订阅产品的任务已开始。"
+      ],
+      "No products selected.": [
+        "没有选择产品."
+      ],
+      "Subscribe SCC Product": [
+        "订阅 SCC 产品"
+      ],
+      "Sync SUSE subscriptions": [
+        "同步 SUSE 订阅"
+      ],
+      "Update SUSE repositories %s": [
+        "更新 SUSE 仓库 %s"
+      ],
+      "Unknown": [
+        "未知"
+      ],
+      "Update SUSE repositories": [
+        "创建 SUSE 仓库"
+      ],
+      "Sync SUSE subscriptions (Products)": [
+        "同步 SUSE 订阅（产品）"
+      ],
+      "Sync SUSE subscriptions (Repositories)": [
+        "同步 SUSE 订阅（仓库）"
+      ],
+      "never synced": [
+        "从未同步"
+      ],
+      "Interval cannot be nil": [
+        "间隔不能为零"
+      ],
+      "Interval not set correctly": [
+        "间隔设置不正确"
+      ],
+      "Cron expression is not valid!": [
+        "Cron 表达式无效！"
+      ],
+      "On Demand": [
+        ""
+      ],
+      "Immediate": [
+        ""
+      ],
+      "Additive": [
+        ""
+      ],
+      "Content Only": [
+        ""
+      ],
+      "SUSE Subscription": [
+        "SUSE 订阅"
+      ],
+      "SUSE Subscription: ": [
+        "SUSE 订阅："
+      ],
+      "SUSE Customer Center account": [
+        "SUSE 客户中心帐户"
+      ],
+      "Use your 'Organization credentials' obtained from the SUSE Customer Center.": [
+        "使用从 SUSE 客户中心获取的\\\"组织凭据\\\"。"
+      ],
+      "Base URL": [
+        "基本 URL"
+      ],
+      "Sync interval": [
+        "同步间隔"
+      ],
+      "The sync interval is used to periodically update the SCC authentication tokens of any imported products.": [
+        "同步间隔用于定期更新任何导入产品的 SCC 身份验证令牌。"
+      ],
+      "Sync Date": [
+        "同步日期"
+      ],
+      "None": [
+        "没有"
+      ],
+      "Use GPG key for SUSE products": [
+        "将 GPG 密钥用于 SUSE 产品"
+      ],
+      "Use this setting if you want to automatically add a GPG key to your SUSE products upon subscription. You can change this setting in the 'Content' > 'Products' menu, later.": [
+        "如果要在订阅时自动在 SUSE 产品中添加 GPG 密钥，请使用这个设置。稍后，您可以在 'Content' > 'Products' 菜单中更改此设置。"
+      ],
+      "Download Policy": [
+        ""
+      ],
+      "Mirroring Policy": [
+        ""
+      ],
+      "Test Connection": [
+        "测试连接"
+      ],
+      "Edit %s": [
+        "编辑 %s"
+      ],
+      "SUSE subscriptions": [
+        "SUSE 订阅"
+      ],
+      "Add SCC account": [
+        "添加 SCC 帐户"
+      ],
+      "Products": [
+        "产品"
+      ],
+      "Last synced": [
+        "最后同步的"
+      ],
+      "Actions": [
+        "操作"
+      ],
+      "Select products": [
+        "选择产品"
+      ],
+      "Sync": [
+        "同步"
+      ],
+      "WARNING: If you want to switch SCC accounts and retain the synchronized content, DO NOT delete your old SCC account, even if it is expired. Please change the login and password of your SCC account, instead.\\n\\nIf you delete your old SCC account, you CANNOT reuse existing repositories, products, content views, and composite content views.\\n\\nReally delete SCC account %s?": [
+        ""
+      ],
+      "Add SUSE Customer Center Account": [
+        "添加 SUSE 客户中心帐户"
+      ],
+      "Product Selection for Account %s": [
+        "为帐户 %s 选择产品"
+      ],
+      "SUSE Subscriptions": [
+        "SUSE 订阅"
+      ],
+      "Please synchronize your SUSE account before you can subscribe to SUSE products.": [
+        ""
+      ],
+      "SUSE Customer Center": [
+        "SUSE 客户中心"
+      ],
+      "Synchronize SUSE Account": [
+        ""
+      ],
+      "The subscription task has been started successfully": [
+        ""
+      ],
+      "Starting the subscription task failed.": [
+        ""
+      ],
+      "Failed to add task to queue.": [
+        ""
+      ],
+      "%s/%s": [
+        ""
+      ],
+      "Filter repositories": [
+        ""
+      ],
+      "If this option is enabled, debug and source pool repositories are automatically selected if you select a product. This option is disabled by default. It applies for unselected products, only. Already selected products are not filtered.": [
+        ""
+      ],
+      "Include Debug and Source Pool repositories": [
+        ""
+      ],
+      "Add product(s)": [
+        ""
+      ],
+      " -- Select Product --": [
+        ""
+      ],
+      " -- Select Version --": [
+        ""
+      ],
+      " -- Select Architecture --": [
+        ""
+      ],
+      "Select SUSE products": [
+        ""
+      ],
+      "Search": [
+        ""
+      ],
+      "Reset Selection": [
+        ""
+      ],
+      "Summary of SCC product subscription": [
+        ""
+      ],
+      "The subscription task with id ": [
+        ""
+      ],
+      " has started successfully.": [
+        ""
+      ],
+      "The following products will be imported:": [
+        ""
+      ],
+      "Go to Repository page": [
+        ""
+      ],
+      "Repository not imported": [
+        ""
+      ],
+      "Please check your SUSE subscription": [
+        ""
+      ],
+      "Repositories (%s/%s)": [
+        ""
+      ],
+      "Go to Product page": [
+        ""
+      ],
+      "Add more sub products to Product tree": [
+        ""
+      ],
+      "Show currently added repositories": [
+        ""
+      ],
+      "Selected SUSE Products": [
+        ""
+      ],
+      "You currently have no SUSE products selected. Search and add SUSE products in the section below.": [
+        ""
+      ],
+      "Show all subscription tasks": [
+        ""
+      ],
+      "Show last product subscription task": [
+        ""
+      ],
+      "Expand products": [
+        ""
+      ],
+      "Collapse products": [
+        ""
+      ],
+      "Collapse/Expand": [
+        ""
+      ],
+      "Show all products": [
+        ""
+      ],
+      "Show only subscribed products": [
+        ""
+      ],
+      "Show/Hide unsubscribed": [
+        ""
+      ],
+      "Import": [
+        ""
+      ],
+      "Export Repository": [
+        ""
+      ],
+      "Export Library": [
+        ""
+      ],
+      "Generate host applicability": [
+        ""
+      ],
+      "Bulk generate applicability for hosts": [
+        ""
+      ],
+      "Generate repository applicability": [
+        ""
+      ],
+      "Synchronize smart proxy": [
+        ""
+      ],
+      "Sync capsule": [
+        ""
+      ],
+      "Update CDN Configuration": [
+        ""
+      ],
+      "Update": [
+        ""
+      ],
+      "Delete": [
+        ""
+      ],
+      "Errata mail": [
+        ""
+      ],
+      "Incremental Update of  Content View Version(s) ": [
+        ""
+      ],
+      "Import facts": [
+        ""
+      ],
+      "Import Puppet classes": [
+        ""
+      ],
+      "Abstract async task": [
+        ""
+      ],
+      "Syncable export": [
+        ""
+      ],
+      "Export": [
+        ""
+      ],
+      "Copy version units to library": [
+        ""
+      ],
+      "Create Syncable Export History": [
+        ""
+      ],
+      "Create Import History": [
+        ""
+      ],
+      "Create": [
+        ""
+      ],
+      "Delete Activation Key": [
+        ""
+      ],
+      "Create Export History": [
+        ""
+      ],
+      "Agent action": [
+        ""
+      ],
+      "Create Alternate Content Source": [
+        ""
+      ],
+      "Destroy Alternate Content Source": [
+        ""
+      ],
+      "Refresh Alternate Content Source": [
+        ""
+      ],
+      "Update Alternate Content Source": [
+        ""
+      ],
+      "Action with sub plans": [
+        ""
+      ],
+      "Abstract": [
+        ""
+      ],
+      "Remove from Environment": [
+        ""
+      ],
+      "Promote": [
+        ""
+      ],
+      "Promotion to Environment": [
+        ""
+      ],
+      "Publish": [
+        ""
+      ],
+      "Remove Versions and Associations": [
+        ""
+      ],
+      "Remove Version": [
+        ""
+      ],
+      "Import Content View Version": [
+        ""
+      ],
+      "Import Default Content View": [
+        ""
+      ],
+      "Import Repository": [
+        ""
+      ],
+      "Incremental Update": [
+        ""
+      ],
+      "Republish Version Repositories": [
+        ""
+      ],
+      "Delete Lifecycle Environment": [
+        ""
+      ],
+      "Publish Lifecycle Environment Repositories": [
+        ""
+      ],
+      "Attach subscriptions": [
+        ""
+      ],
+      "Auto attach subscriptions": [
+        ""
+      ],
+      "Destroy Content Host": [
+        ""
+      ],
+      "Install Applicable Errata": [
+        ""
+      ],
+      "Hypervisors": [
+        ""
+      ],
+      "Hypervisors update": [
+        ""
+      ],
+      "Remove subscriptions": [
+        ""
+      ],
+      "Combined Profile Update": [
+        ""
+      ],
+      "Update Content Overrides": [
+        ""
+      ],
+      "Update for host": [
+        ""
+      ],
+      "Update release version for host": [
+        ""
+      ],
+      "Updating System Purpose for host": [
+        ""
+      ],
+      "Package Profile Update": [
+        ""
+      ],
+      "Destroy": [
+        ""
+      ],
+      "Run Sync Plan:": [
+        ""
+      ],
+      "Enable": [
+        ""
+      ],
+      "Disable": [
+        ""
+      ],
+      "Verify checksum": [
+        ""
+      ],
+      "Create Package Group": [
+        ""
+      ],
+      "Upload into": [
+        ""
+      ],
+      "Update redhat repository": [
+        ""
+      ],
+      "Update http proxy details": [
+        ""
+      ],
+      "Update content urls": [
+        ""
+      ],
+      "Discover": [
+        ""
+      ],
+      "Synchronize": [
+        ""
+      ],
+      "Remove Content": [
+        ""
+      ],
+      "Instance update": [
+        ""
+      ],
+      "Index package groups": [
+        ""
+      ],
+      "Index module streams": [
+        ""
+      ],
+      "Index errata": [
+        ""
+      ],
+      "Index content": [
+        ""
+      ],
+      "Filtered index content": [
+        ""
+      ],
+      "Fetch pxe files": [
+        ""
+      ],
+      "Delete Package Group": [
+        ""
+      ],
+      "Update http proxy": [
+        ""
+      ],
+      "Reindex subscriptions": [
+        ""
+      ],
+      "Delete Product": [
+        ""
+      ],
+      "Product Create": [
+        ""
+      ],
+      "Foreman plugin to sync SUSE Customer Center products and repositories into Katello.": [
+        "Foreman 插件，将 SUSE 客户中心产品和存储库同步到 Katello。"
+      ]
+    }
+  }
+};
